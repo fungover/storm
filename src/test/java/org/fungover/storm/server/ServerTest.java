16be8134d18aae6757fda71cda339031e974f8ce
ChristianLofqvist <christian.lofqvist@iths.se>
2022-01-31 13:25:11 +0100

    Increase thread sleep even more

--- src/test/java/org/fungover/storm/server/ServerTest.java
+++ src/test/java/org/fungover/storm/server/ServerTest.java
@@ -28,7 +28,7 @@ class ServerTest {
         Executors.newSingleThreadExecutor().submit(() -> new Server(port).start());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
