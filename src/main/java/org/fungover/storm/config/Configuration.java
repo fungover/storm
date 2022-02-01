8446a26024f14ede77b204cc44e945599e4cea67
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-01 14:48:16 +0100

    Added getter and setter in Configuration class

--- src/main/java/org/fungover/storm/config/Configuration.java
+++ src/main/java/org/fungover/storm/config/Configuration.java
@@ -1,4 +1,24 @@
 package org.fungover.storm.config;
 
 public class Configuration {
+    private int port;
+    private String webroot;
+
+    public int getPort() {
+        return port;
+    }
+
+    public Configuration setPort(int port) {
+        this.port = port;
+        return this;
+    }
+
+    public String getWebroot() {
+        return webroot;
+    }
+
+    public Configuration setWebroot(String webroot) {
+        this.webroot = webroot;
+        return this;
+    }
 }
