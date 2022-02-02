2b78dda4c4b4cd2c83259b0b701c51af6be37f93
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-02 19:45:00 +0100

    Added methods in ConfigurationManager

--- src/main/java/org/fungover/storm/config/ConfigurationManager.java
+++ src/main/java/org/fungover/storm/config/ConfigurationManager.java
@@ -1,4 +1,59 @@
 package org.fungover.storm.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
+
+    private static ConfigurationManager myConfigurationManager;
+    private static Configuration myCurrentConfiguration;
+
+    private ConfigurationManager() {
+    }
+
+    public static ConfigurationManager getInstance(){
+        if (myConfigurationManager == null)
+            myConfigurationManager = new ConfigurationManager();
+        return myConfigurationManager;
+    }
+
+    public void readConfigurationFile(String filePath) {
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new MyConfigurationException(e);
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        int i;
+        try {
+            while (( i = fileReader.read()) != -1) {
+            stringBuffer.append((char) i);
+            }
+        } catch (IOException e) {
+            throw new MyConfigurationException(e);
+        }
+        JsonNode conf;
+        try {
+            conf = Json.parse(stringBuffer.toString());
+        } catch (JsonProcessingException e) {
+            throw new MyConfigurationException("Error parsing the Configuration File", e);
+        }
+        try {
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new MyConfigurationException("Error parsing the Configuration file, internal", e);
+        }
+    }
+
+    public Configuration getCurrentConfiguration(){
+        if (myCurrentConfiguration == null) {
+            throw new MyConfigurationException("No Current Configuration Set.");
+        }
+        return myCurrentConfiguration;
+    }
 }
