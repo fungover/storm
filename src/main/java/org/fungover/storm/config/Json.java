97aa428f34a6da485c8a614293045a7c497f4094
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-02 08:19:14 +0100

    Added methods in Json class

--- src/main/java/org/fungover/storm/config/Json.java
+++ src/main/java/org/fungover/storm/config/Json.java
@@ -1,5 +1,23 @@
 package org.fungover.storm.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+
 public class Json {
 
+    private static ObjectMapper myObjectMapper = defaultObjectMapper();
+
+    private static ObjectMapper defaultObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
+    public static JsonNode parse(String jsonSrc) throws JsonProcessingException {
+        return myObjectMapper.readTree(jsonSrc);
+    }
+
+    public static <A> A fromJson(JsonNode node, Class<A> clazz) throws JsonProcessingException {
+        return myObjectMapper.treeToValue(node, clazz);
+    }
 }
