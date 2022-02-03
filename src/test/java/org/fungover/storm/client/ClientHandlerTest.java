0406afdeb1c869d849134f9b6f591ab110cfc770
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-03 11:33:23 +0100

    Add test for ClientHandler when there is a connection error

--- src/test/java/org/fungover/storm/client/ClientHandlerTest.java
+++ src/test/java/org/fungover/storm/client/ClientHandlerTest.java
@@ -1,13 +1,16 @@
 package org.fungover.storm.client;
 
+import nl.altindag.log.LogCaptor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.io.*;
 import java.net.Socket;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.Mockito.mock;
+
 
 class ClientHandlerTest {
 
@@ -21,6 +24,18 @@ class ClientHandlerTest {
         ClientHandler clientHandler = new ClientHandler(socket);
 
         assertThatNoException().isThrownBy(clientHandler::run);
+    }
+
+    @Test
+    void shouldLogAnErrorWhenThereIsAConnectionError() throws IOException {
+        LogCaptor logCaptor = LogCaptor.forName("SERVER");
+        Socket socket = new Socket();
+        ClientHandler clientHandler = new ClientHandler(socket);
+
+        socket.close();
+        clientHandler.run();
+
+        assertThat(logCaptor.getErrorLogs()).hasSize(1);
     }
 
 }
