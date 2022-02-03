3a9be27476d7f1c55b00fd65a1c71e4600dc8a40
Joel <dev.joejoh@gmail.com>
2022-02-03 18:26:16 +0100

    Add testcase for index request

--- src/test/java/org/fungover/storm/fileHandler/FileRequestHandlerTest.java
+++ src/test/java/org/fungover/storm/fileHandler/FileRequestHandlerTest.java
@@ -33,4 +33,26 @@ public class FileRequestHandlerTest {
 
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void indexPathShouldPointTowardsIndex() throws IOException {
+        String req = """
+                GET /index.html HTTP/1.1
+                Host: storm.fungover.org
+                Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+                Accept-Language: en-US,en;q=0.5
+                Connection: keep-alive
+                """;
+        FileRequestHandler.handleRequest(req);
+        byte[][] result = FileRequestHandler.writeResponse();
+        Path path = FileRequestHandler.getPath();
+        byte[] file = Files.readAllBytes(path);
+        String response = "HTTP/1.1 200 OK \r\nContent-length:" + file.length +
+                "Content-type:" + FormatConverter.MIME(path) +
+                "\r\nConnection: Closed\r\n\r\n";
+        byte[][] expected = new byte[][]{response.getBytes(), file};
+
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
