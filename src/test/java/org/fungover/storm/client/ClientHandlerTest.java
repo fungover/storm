43f839d5de98a6d24494fd2102b6eb5f3a2ea2d8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-03 11:56:23 +0100

    Refactor ClientHandlerTest

--- src/test/java/org/fungover/storm/client/ClientHandlerTest.java
+++ src/test/java/org/fungover/storm/client/ClientHandlerTest.java
@@ -8,22 +8,22 @@ import java.io.*;
 import java.net.Socket;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.Mockito.mock;
 
 
 class ClientHandlerTest {
 
     @Test
-    void shouldNotThrowExceptionWhenThereIsASuccessfulConnection() throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        InputStream inputStream = new ByteArrayInputStream("hello world".getBytes());
+    void shouldNotLogAnErrorWhenThereIsASuccessfulConnection() throws IOException {
+        LogCaptor logCaptor = LogCaptor.forName("SERVER");
         Socket socket = mock(Socket.class);
-        Mockito.when(socket.getOutputStream()).thenReturn(outputStream);
-        Mockito.when(socket.getInputStream()).thenReturn(inputStream);
+        Mockito.when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+        Mockito.when(socket.getInputStream()).thenReturn(new ByteArrayInputStream("Hello Storm!".getBytes()));
         ClientHandler clientHandler = new ClientHandler(socket);
 
-        assertThatNoException().isThrownBy(clientHandler::run);
+        clientHandler.run();
+
+        assertThat(logCaptor.getErrorLogs()).isEmpty();
     }
 
     @Test
