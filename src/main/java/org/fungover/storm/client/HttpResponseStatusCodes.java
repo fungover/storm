48d1ccffba1b0eb856501bef610303126fae917c
albindubech <albin.dubech@hotmail.com>
2022-02-04 12:07:04 +0100

    Remove superfluous text from error500 String

--- src/main/java/org/fungover/storm/client/HttpResponseStatusCodes.java
+++ src/main/java/org/fungover/storm/client/HttpResponseStatusCodes.java
@@ -1,7 +1,7 @@
 package org.fungover.storm.client;
 
 public class HttpResponseStatusCodes {
-    static final String error500 = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+    static final String error500 = "500 Internal Server Error\r\n";
 
     String getError500() {
         return error500;
