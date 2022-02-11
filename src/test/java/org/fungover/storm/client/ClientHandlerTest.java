8191cc4ff7b1666723c0a23bd783b23417dc87ab
Philippe <philippe.vial90@gmail.com>
2022-02-11 12:31:37 +0100

    Create ClientHandlerTest and add test for 500 error

---
+++ src/test/java/org/fungover/storm/client/ClientHandlerTest.java
@@ -0,0 +1,34 @@
+package org.fungover.storm.client;
+
+import nl.altindag.log.LogCaptor;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fungover.storm.fileHandler.re.ResponseCode.ERROR_500;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class ClientHandlerTest {
+
+
+    @Test
+    void runShouldThrow500ErrorMessage() throws IOException {
+
+
+        Socket clientSocket = Mockito.mock(Socket.class);
+        Mockito.when(clientSocket.getOutputStream()).thenThrow(new RuntimeException(ERROR_500.getCode()));
+        Throwable exceptionThatWasThrown = assertThrows(RuntimeException.class, () -> {
+            clientSocket.getOutputStream();
+        });
+
+        assertThat(exceptionThatWasThrown.getMessage()).isEqualTo(ERROR_500.getCode());
+
+    }
+}
