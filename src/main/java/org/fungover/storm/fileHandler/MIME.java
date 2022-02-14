91c9996e822b4dd37d177ff44f43a9c9a8d407ae
Martin Blomberg <martin.blomberg@outlook.com>
2022-02-14 18:43:52 +0100

    Remove unnecessary methods in MIME

--- src/main/java/org/fungover/storm/fileHandler/MIME.java
+++ src/main/java/org/fungover/storm/fileHandler/MIME.java
@@ -1,13 +1,4 @@
 package org.fungover.storm.fileHandler;
 
 public record MIME(String type, String subtype) {
-    @Override
-    public String type() {
-        return type;
-    }
-
-    @Override
-    public String subtype() {
-        return subtype;
-    }
 }
